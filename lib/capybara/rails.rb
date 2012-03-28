aff0e6881a0f43bbe886eb9ad3bb8b8682a6384e
Veezus Kreist <almighty@veez.us>
2012-03-28 13:11:34 -0400

    Add rails 2.2.2 compatibility

--- lib/capybara/rails.rb
+++ lib/capybara/rails.rb
@@ -12,6 +12,6 @@ Capybara.app = Rack::Builder.new do
   end
 end.to_app
 
-Capybara.asset_root = Rails.root.join('public')
-Capybara.save_and_open_page_path = Rails.root.join('tmp/capybara')
+Capybara.asset_root = File.join(Rails.root, 'public')
+Capybara.save_and_open_page_path = File.join(Rails.root, 'tmp', 'capybara')
 
